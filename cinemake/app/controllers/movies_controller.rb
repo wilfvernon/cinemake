a1c063c0eee57ceebb0d1f3abd734e01c86f8ecf
github username <github email address>
2019-10-01 15:53:18 -0400

    building up movie

---
+++ cinemake/app/controllers/movies_controller.rb
@@ -0,0 +1,18 @@
+class MoviesController < ApplicationController
+
+    def index
+        @movies = Movie.all
+    end
+
+    def show
+        @movie = Movie.find(params[:id])
+    end
+    
+    def new
+        @movie = Movie.new
+    end
+
+    def create
+        @movie = Movie.new(movie_params)
+    end
+end
