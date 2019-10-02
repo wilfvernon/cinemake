41635017e271472d81c8e4675ebb267086b912ea
Eugenia Kim <elk303@nyu.edu>
2019-10-02 13:15:33 -0400

    merge

--- cinemake/app/controllers/crew_members_controller.rb
+++ cinemake/app/controllers/crew_members_controller.rb
@@ -21,7 +21,7 @@ class CrewMembersController < ApplicationController
             @crew_member = CrewMember.find(params["crew_member"]["id"])
             redirect_to new_role_path
         else 
-            CrewMember.create(:id => params["crew_member"]["id"], :birthday => params["crew_member"]["birthday"], :gender_id => params["crew_member"]["gender_id"], :place_of_birth => params["crew_member"]["place_of_birth"], :bio => params["crew_member"]["bio"], :name => params["crew_member"]["name"], :crew_member_type => params["crew_member"]["crew_member_type"], :image => params["crew_member"]["profile_path"])
+            @crew_member = CrewMember.create(:id => params["crew_member"]["id"], :birthday => params["crew_member"]["birthday"], :gender_id => params["crew_member"]["gender_id"], :place_of_birth => params["crew_member"]["place_of_birth"], :bio => params["crew_member"]["bio"], :name => params["crew_member"]["name"], :crew_member_type => params["crew_member"]["crew_member_type"], :image => params["crew_member"]["profile_path"])
             redirect_to new_role_path
         end
     end
